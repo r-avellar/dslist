96dfaa6b02993943c8039a0f4c3fe4c1acba2ff9
r-avellar <renatoavellar5@gmail.com>
2023-05-14 21:14:18 -0300

    entity game list

---
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.controllers;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/games")
+public class GameController {
+    @Autowired
+    private GameService service;
+
+    @GetMapping
+    public List<GameMinDTO> listAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO listById(@PathVariable Long id){
+        return service.findById(id);
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslist.repositories;
+
+import com.devsuperior.dslist.entites.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
+}
